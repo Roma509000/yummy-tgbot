ee64c41a798ff0b670b308d1e44860ebc8b08b13
Lielem <r3ynieasos@gmail.com>
2022-02-08 16:40:21 +0300

    some fix

--- yummy_create_db.py
+++ yummy_create_db.py
@@ -5,7 +5,7 @@ connection = sqlite3.connect('yummy_database.db')
 
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE yummybot (name TEXT, rate TEXT, status TEXT, year TEXT, categories TEXT, type TEXT, description TEXT, url TEXT, poster_url TEXT, desc_page TEXT)")
+cursor.execute("CREATE TABLE yummybot (name TEXT, rate REAL, status TEXT, year TEXT, categories TEXT, type TEXT, description TEXT, url TEXT, poster_url TEXT")
 
 connection.close()
 
